5e6614897b353d1c60e07cbcf144283068712f0b
JacobFeat <jakubk198@gmail.com>
2021-06-24 18:01:10 +0200

    fix change bg function

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const changeBtnAll = document.querySelector(".change-bg-btn");
+const changeBtnAll = document.querySelectorAll(".change-bg-btn")
+;
+
 const changeFirBtn = document.querySelector(".change-first");
 const changeSecBtn = document.querySelector(".change-second");
 const changeThiBtn = document.querySelector(".change-third");
@@ -8,11 +10,11 @@ const thirdBgImg = document.querySelector(".third-img");
 
 
 changeFirBtn.addEventListener("click", turnFirstOn);
-
 changeSecBtn.addEventListener("click", turnSecondOn);
-
 changeThiBtn.addEventListener("click", turnThirdOn);
 
+setTimeout(turnSecondOn, 7000);
+setTimeout(turnThirdOn, 14000);
 setInterval(()=>{
     setTimeout(turnSecondOn, 7000);
     setTimeout(turnThirdOn, 14000);
@@ -23,16 +25,25 @@ function turnFirstOn(){
     firstBgImg.style.opacity="1";
     secondBgImg.style.opacity="0";
     thirdBgImg.style.opacity="0";
+    changeFirBtn.style.background="#C8C8C8";
+    changeSecBtn.style.background="#EAEAEA";
+    changeThiBtn.style.background="#EAEAEA";
 }
 
 function turnSecondOn(){
     firstBgImg.style.opacity="0";
     secondBgImg.style.opacity="1";
     thirdBgImg.style.opacity="0";
+    changeFirBtn.style.background="#EAEAEA";
+    changeSecBtn.style.background="#C8C8C8";
+    changeThiBtn.style.background="#EAEAEA";
 }
 
 function turnThirdOn(){
     firstBgImg.style.opacity="0";
     secondBgImg.style.opacity="0";
     thirdBgImg.style.opacity="1";
+    changeFirBtn.style.background="#EAEAEA";
+    changeSecBtn.style.background="#EAEAEA";
+    changeThiBtn.style.background="#C8C8C8";
 }
